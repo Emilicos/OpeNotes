37fdc9b251eec95fe9fe995130679dc84f3e2bfe
emilicos <notalvaroaustin@gmail.com>
2023-11-26 01:39:36 +0700

    feat: add tailwind and templates

---
+++ README.md
@@ -0,0 +1,16 @@
+## For OpeNotes members:
+This is base templates for html
+```
+{% extends 'base.html' %} 
+{% load static %} 
+
+{% block styles %}
+{% endblock styles %}
+
+{% block content %}
+{% comment %}
+  Add something here, up to you (remove the comment and endcomment block)
+{% endcomment %}
+
+{% endblock content %}
+```
--- authentication/urls.py
+++ authentication/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from authentication.views import show_home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("auth/", include("authentication.urls"))
+    path('', show_home, name='home'),
 ]
