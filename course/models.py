14f2494b6e1ab11bd477a31996689f69e5c26743
emilicos <notalvaroaustin@gmail.com>
2023-11-27 01:28:26 +0700

    feat: finish api logic for course

--- course/models.py
+++ course/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+    credit = models.IntegerField(default = 0)
+    description = models.TextField(default = "")
+    semester = models.IntegerField(default = 0)
+    year = models.IntegerField(default = 0)
+    
+    def __str__(self):
+        return self.name
--- course/urls.py
+++ course/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from course.views import CourseListView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("landing.urls")),
-    path("api/auth/", include("authentication.urls")),
-    path("course/", include("course.urls")),
+    path("course/", CourseListView.as_view()),
+    path("course/<str:id>/", CourseListView.as_view()),
 ]
