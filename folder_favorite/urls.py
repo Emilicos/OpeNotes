6b79ce646afe8780f508c6f6fde1381584d4f5c5
Febrian Dwi Kimhan <febrian.dwi11@ui.ac.id>
2023-11-30 13:24:26 +0700

    restore folder favorite

--- folder_favorite/urls.py
+++ folder_favorite/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from .views import FolderFavoriteListView, FolderFavoriteDetailView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("landing.urls")),
-    path("api/auth/", include("authentication.urls")),
-    path("course/", include("course.urls")),
-    path("course/notes/", include("notes.urls")),
-    path("favorite/", include("folder_favorite.urls")),
+    path("", FolderFavoriteListView.as_view()),
+    path("<str:id>/", FolderFavoriteDetailView.as_view()),
 ]
--- openotes/settings.py
+++ openotes/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'landing',
     'course',
     'notes',
+    'folder_favorite',
 ]
 
 MIDDLEWARE = [
