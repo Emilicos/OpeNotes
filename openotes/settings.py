394b0e8542db36949f2391ad06ebeb0d6f74f6e4
emilicos <notalvaroaustin@gmail.com>
2023-12-03 16:52:56 +0700

    feat: add static root

--- openotes/settings.py
+++ openotes/settings.py
@@ -150,6 +150,7 @@ cloudinary.config(
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_URL = 'static/'
 
